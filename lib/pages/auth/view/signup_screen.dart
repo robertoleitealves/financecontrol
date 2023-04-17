b7c8ceaa4096ca45eafca9d37b1f29c8b90d3e58
robertoleitealves <69829005+robertoleitealves@users.noreply.github.com>
2023-04-17 11:34:28 -0300

    Configure SignUp Screen

--- lib/pages/auth/view/signup_screen.dart
+++ lib/pages/auth/view/signup_screen.dart
@@ -23,93 +23,95 @@ class _SignUpScreenState extends State<SignUpScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.only(top: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: CustomTextField(
-                  label: 'Nome',
-                  icon: Icons.person,
-                  controller: _controller.nameController),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: CustomTextField(
-                label: 'Data de nascimento',
-                icon: Icons.date_range,
-                controller: _controller.dataNascController,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: CustomTextField(
+                    label: 'Nome',
+                    icon: Icons.person,
+                    controller: _controller.nameController),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: CustomTextField(
-                label: 'Telefone',
-                icon: Icons.phone,
-                controller: _controller.phoneNumberController,
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: CustomTextField(
+                  label: 'Data de nascimento',
+                  icon: Icons.date_range,
+                  controller: _controller.dataNascController,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: CustomTextField(
-                label: 'CPF',
-                icon: Icons.numbers,
-                controller: _controller.cpfController,
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: CustomTextField(
+                  label: 'Telefone',
+                  icon: Icons.phone,
+                  controller: _controller.phoneNumberController,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: CustomTextField(
-                label: 'Digite uma senha',
-                icon: Icons.numbers,
-                controller: _controller.passwordController,
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: CustomTextField(
+                  label: 'CPF',
+                  icon: Icons.numbers,
+                  controller: _controller.cpfController,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: CustomTextField(
-                label: 'Digite a senha novamente',
-                icon: Icons.numbers,
-                controller: _controller.passwordConfirmController,
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: CustomTextField(
+                  label: 'Digite uma senha',
+                  icon: Icons.numbers,
+                  controller: _controller.passwordController,
+                ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: CustomColors.customSwatchColor,
-                  padding: const EdgeInsets.all(8.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: CustomTextField(
+                  label: 'Digite a senha novamente',
+                  icon: Icons.numbers,
+                  controller: _controller.passwordConfirmController,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: CustomColors.customSwatchColor,
+                    padding: const EdgeInsets.all(8.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                  child: const Text(
+                    'Cadastrar nova conta',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                 ),
-                child: const Text(
-                  'Cadastrar nova conta',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(8.0),
-                  backgroundColor: CustomColors.customContrastColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(8.0),
+                    backgroundColor: CustomColors.customContrastColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                  child: const Text(
+                    'Voltar',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                 ),
-                child: const Text(
-                  'Voltar',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
